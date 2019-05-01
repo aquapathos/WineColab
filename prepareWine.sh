bf5b9275b64a28458c7cd43b583071f161074efa
aquapathos <firedwater@gmail.com>
2019-05-01 14:34:33 +0900

    Update prepareWine.sh

--- prepareWine.sh
+++ prepareWine.sh
@@ -98,4 +98,4 @@ keycode 113 = Left
 keycode 114 = Right
 EOS
 python /opt/websockify/run 5901 --web=/opt/noVNC --wrap-mode=ignore -- /opt/TurboVNC/bin/vncserver :1 -depth 24 -geometry 1600x900 -securitytypes otp -otp -noxstartup > /content/.vnc/stdout 2>&1 &
-xmodmap ~/.Xmodmap
+DISPLAY =:1 xmodmap ~/.Xmodmap
